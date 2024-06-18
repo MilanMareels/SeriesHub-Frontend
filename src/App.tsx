ff0876e8702d3fe5dbcd6e54bcbd5017b4f7fd99
MilanMareels <milan.mareels@gmail.com>
2024-06-18 21:30:55 +0200

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,8 @@ function App() {
               <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/mySeries" element={<Login />} />
+                <Route path="/profile" element={<Login />} />
               </Routes>
             ) : (
               <>
