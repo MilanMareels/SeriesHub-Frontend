4455a2ebd50cfc81475b59766d8deb42116b05fa
MilanMareels <milan.mareels@gmail.com>
2024-06-18 17:46:51 +0200

    id to userId

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,42 +1,46 @@
 import React, { useState } from "react";
 
 interface AuthContextType {
-	isAuthenticated: boolean;
-	userId: string;
-	setUserId: (userId: string) => void;
-	login: (userId: string) => void;
-	logout: () => void;
+  isAuthenticated: boolean;
+  userId: string;
+  setUserId: (userId: string) => void;
+  login: (userId: string) => void;
+  logout: () => void;
 }
 
 export const AuthContext = React.createContext<AuthContextType>({
-	isAuthenticated: false,
-	userId: "",
-	login: () => {},
-	logout: () => {},
-	setUserId: () => {}
+  isAuthenticated: false,
+  userId: "",
+  login: () => {},
+  logout: () => {},
+  setUserId: () => {},
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
-		return localStorage.getItem("isAuthenticated") === "true";
-	});
-	const [userId, setUserId] = useState<string>(() => {
-		return localStorage.getItem("userId") || "";
-	});
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(() => {
+    return localStorage.getItem("isAuthenticated") === "true";
+  });
+  const [userId, setUserId] = useState<string>(() => {
+    return localStorage.getItem("userId") || "";
+  });
 
-	const login = (id: string) => {
-		setIsAuthenticated(true);
-		setUserId(id);
-		localStorage.setItem("isAuthenticated", "true");
-		localStorage.setItem("userId", id);
-	};
+  const login = (userId: string) => {
+    setIsAuthenticated(true);
+    setUserId(userId);
+    localStorage.setItem("isAuthenticated", "true");
+    localStorage.setItem("userId", userId);
+  };
 
-	const logout = () => {
-		setIsAuthenticated(false);
-		setUserId("");
-		localStorage.removeItem("isAuthenticated");
-		localStorage.removeItem("userId");
-	};
+  const logout = () => {
+    setIsAuthenticated(false);
+    setUserId("");
+    localStorage.removeItem("isAuthenticated");
+    localStorage.removeItem("userId");
+  };
 
-	return <AuthContext.Provider value={{ isAuthenticated, userId, setUserId, login, logout }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, userId, setUserId, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
