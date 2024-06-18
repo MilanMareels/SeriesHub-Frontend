df93f71ff1ffe14fe76f27c0a833224f82bb727d
MilanMareels <125263673+MilanMareels@users.noreply.github.com>
2024-06-18 19:59:08 +0200

    register user done (#6)

---
+++ src/components/register/RegisterHeader.tsx
@@ -0,0 +1,10 @@
+const RegisterHeader = () => {
+  return (
+    <>
+      <h2 className="text-center text-3xl font-extrabold text-white">Welcome</h2>
+      <p className="mt-4 text-center text-gray-400">Sign up to continue</p>
+    </>
+  );
+};
+
+export default RegisterHeader;
