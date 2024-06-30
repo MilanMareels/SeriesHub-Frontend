cd6e353ee3a471a2db32065d7c5af59e4c1ac206
MilanMareels <milan.mareels@gmail.com>
2024-06-30 20:34:13 +0200

    fixed type problem

--- src/components/animeDetailPage/animeTrailerSection.tsx
+++ src/components/animeDetailPage/animeTrailerSection.tsx
@@ -16,7 +16,7 @@ const AnimeTrailerSection = ({ animeDetailSeries }: AnimeTrailerSectionProps) =>
         <div className="md:w-11/12 w-full bg-white flex justify-center items-center rounded-lg">
           <div className="flex flex-col items-center justify-center gap-5 p-5">
             <CircleX size={50} color="red" />
-            <h1 className="text-xl">Oops! No trailer found</h1>
+            <h1 className="text-xl">No trailer found</h1>
             <p className="text-center">
               It seems that there is currently no YouTube trailer available for this title. Please try again later or check out the other anime's and
               content available on our site.
