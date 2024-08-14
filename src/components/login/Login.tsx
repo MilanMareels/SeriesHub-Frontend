a654144a2a5b4c129874c0a9b131831ab0964521
MilanMareels <milan.mareels@gmail.com>
2024-08-14 15:14:55 +0200

    fixed heights

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -34,18 +34,12 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-svh flex justify-center items-center">
       <div className="max-w-lg w-full m-6">
         <div className="box-shadow: 0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04); bg-gray-800 rounded-lg shadow-xl overflow-hidden">
           <div className="p-8">
             <FormHeader title="Welcome Back" text="Sign in to continue" />
-            <LoginForm
-              handleLogin={handleLogin}
-              setPassword={setPassword}
-              setText={setText}
-              errorMessage={errorMessage!}
-              loading={loading}
-            />
+            <LoginForm handleLogin={handleLogin} setPassword={setPassword} setText={setText} errorMessage={errorMessage!} loading={loading} />
           </div>
 
           <FormBottom text="Don't have an account? " to="/register" linkText="Sign Up" />
