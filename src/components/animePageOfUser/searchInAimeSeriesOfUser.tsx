339cb5ca5526e93f321cafc3f3c628654294acc1
MilanMareels <milan.mareels@gmail.com>
2024-08-15 13:43:35 +0200

    added filter context for list status selectors and pagination

--- src/components/animePageOfUser/searchInAimeSeriesOfUser.tsx
+++ src/components/animePageOfUser/searchInAimeSeriesOfUser.tsx
@@ -1,12 +1,17 @@
-import { Search } from "lucide-react";
+import { FilterContext } from "@/contexts/FilterContext";
+import { useContext } from "react";
 
 const SearchInAnimeSeriesOfUser = () => {
+  const { search, setSearch } = useContext(FilterContext);
   return (
     <div className="flex gap-1">
-      <input type="text" className="p-2.5 w-64 bg-[#1B1A55] rounded-lg placeholder:text-white focus:text-white text-white" placeholder="Find your anime..." />
-      <button className="p-2.5 bg-[#1B1A55] rounded-lg text-white transition duration-150 hover:scale-110 ease-in-out">
-        <Search />
-      </button>
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        className="p-2.5 w-64 bg-[#1B1A55] rounded-lg placeholder:text-white focus:text-white text-white"
+        placeholder="Find your anime..."
+      />
     </div>
   );
 };
